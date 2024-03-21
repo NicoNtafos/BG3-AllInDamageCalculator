728ea13095c70d8c0cef4bceca69e93d623ff6c0
NicoNtafos <nicholasntafos@gmail.com>
2024-03-20 21:22:58 -0700

    Outlining basic program flow

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
+import sys
+
+
 def main():
+
+    prompt_user()
+
+
+def prompt_user():
+
+    print("Do you want to perform a damage calculation or adjust your damage dice?")
+    print("1. Damage Calculation")
+    print("2. Adjust Damage Dice")
+    print("3. Exit Program")
+
+    operation = str(input())
+
+    if operation != "1" and operation != "2" and operation != "3":
+        raise Exception("Operation input invalid - select 1, 2, or 3")
+    print("")
+
+    if operation == "1":
+        calculate_dmg()
+    elif operation == "2":
+        adjust_dice()
+    else:
+        sys.exit(0)
+
+# Checks that accuracy input is valid, used in calculate_dmg()
+def check_accuracy(accuracy):
+    if accuracy < 1 or accuracy > 100:
+        raise Exception("Accuracy input invalid - must be between 1 and 100")
+
+
+# TODO - Implement below method
+def calculate_kill_chance(min_dmg, max_dmg):
+    return "WIP"
+
+
+def calculate_dmg():
     print("What is your chance to hit with sharpshooter? (Enter w/o percent sign): ")
     ss_accuracy = int(input())
     check_accuracy(ss_accuracy)
@@ -31,17 +70,18 @@ def main():
 
     print("Expected damage change with sharpshooter: " + str(ss_dmg_diff))
 
-    #TODO - Add calculation for kill chance given enemy HP
-    #Tested above calculation and it works as expected
+    # TODO - Add calculation for kill chance given enemy HP
+    # Tested above calculation and it works as expected
+
+    print("")
+    prompt_user()
 
 
-def check_accuracy(accuracy):
-    if accuracy < 1 or accuracy > 100:
-        raise Exception("Accuracy input invalid - must be between 1 and 100")
-
-# TODO - Implement below method
-def calculate_kill_chance(min_dmg, max_dmg):
-    return "WIP"
+# TODO - Finish implementing
+def adjust_dice():
+    print("WIP")
+    print("")
+    prompt_user()
 
 
 if __name__ == "__main__":
