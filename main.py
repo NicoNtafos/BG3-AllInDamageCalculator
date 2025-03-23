4a65684161096060e0c563b9666d4167e0a74854
NicoNtafos <nicholasntafos@gmail.com>
2025-03-23 10:02:40 -0700

    Refactoring, implementing kill chance calculation

--- main.py
+++ main.py
@@ -1,58 +1,39 @@
 import sys
-
+import numpy as np
 
 def main():
-
     prompt_user()
 
-
 def prompt_user():
-
-    print("Do you want to perform a damage calculation or adjust your damage dice?")
-    print("1. Damage Calculation")
-    print("2. Adjust Damage Dice")
-    print("3. Exit Program")
+    print("Press 1 to start a new damage calculation or 2 to quit")
 
     operation = str(input())
 
-    if operation != "1" and operation != "2" and operation != "3":
-        raise Exception("Operation input invalid - select 1, 2, or 3")
+    if operation != "1" and operation != "2":
+        raise Exception("Input invalid - select 1 or 2")
     print("")
 
     if operation == "1":
         calculate_dmg()
-    elif operation == "2":
-        adjust_dice()
     else:
         sys.exit(0)
 
-# Checks that accuracy input is valid, used in calculate_dmg()
-def check_accuracy(accuracy):
-    if accuracy < 1 or accuracy > 100:
-        raise Exception("Accuracy input invalid - must be between 1 and 100")
-
-
-# TODO - Implement below method
-def calculate_kill_chance(min_dmg, max_dmg):
-    return "WIP"
-
-
 def calculate_dmg():
-    print("What is your chance to hit with sharpshooter? (Enter w/o percent sign): ")
-    ss_accuracy = int(input())
-    check_accuracy(ss_accuracy)
-    ss_accuracy /= 100
-
-    print("What is your chance to hit without sharpshooter? (Enter w/o percent sign): ")
-    base_accuracy = int(input())
-    check_accuracy(base_accuracy)
-    base_accuracy /= 100
-
-    print("Without sharpshooter, what is the bottom of your damage range?")
+    print("What is the bottom of your damage range with \"All In\" OFF?")
     base_min_dmg = int(input())
 
-    print("Without sharpshooter, what is the top of your damage range?")
+    print("What is the top of your damage range with \"All In\" OFF?")
     base_max_dmg = int(input())
+
+    print("What is your chance to hit with \"All In\" OFF? (Enter w/o percent sign): ")
+    base_hit_chance = int(input())
+    check_hit_chance(base_hit_chance)
+    base_hit_chance /= 100
+
+    print("What is your chance to hit with \"All In\" ON? (Enter w/o percent sign): ")
+    ss_hit_chance = int(input())
+    check_hit_chance(ss_hit_chance)
+    ss_hit_chance /= 100
 
     print("What is your target's HP?")
     target_hp = int(input())
@@ -63,26 +44,32 @@ def calculate_dmg():
     base_hit_dmg = (base_min_dmg + base_max_dmg) / 2
     ss_hit_dmg = (ss_min_dmg + ss_max_dmg) / 2
 
-    base_expected_dmg = base_accuracy * base_hit_dmg
-    ss_expected_dmg = ss_accuracy * ss_hit_dmg
+    print("Average expected damage with \"All In\" ON: " + str(round(ss_hit_chance * ss_hit_dmg, 2)))
+    print("Average expected damage with \"All In\" OFF: " + str(round(base_hit_chance * base_hit_dmg, 2)))
 
-    ss_dmg_diff = ss_expected_dmg - base_expected_dmg
-
-    print("Expected damage change with sharpshooter: " + str(ss_dmg_diff))
-
-    # TODO - Add calculation for kill chance given enemy HP
-    # Tested above calculation and it works as expected
+    # TODO - add unit tests - manually tested all calculations except kill chance and it works as expected
+    print("Kill chance with \"All In\" ON: " + str(round(calculate_kill_chance(ss_min_dmg, ss_max_dmg, target_hp, ss_hit_chance), 2)) + "%")
+    print("Kill chance with \"All In\" OFF: " + str(round(calculate_kill_chance(base_min_dmg, base_max_dmg, target_hp, base_hit_chance), 2)) + "%")
 
     print("")
     prompt_user()
 
+# Checks that hit chance input is valid, used in calculate_dmg()
+def check_hit_chance(hit_chance):
+    if hit_chance < 1 or hit_chance > 100:
+        raise Exception("Hit chance input invalid - must be between 1 and 100")
 
-# TODO - Finish implementing
-def adjust_dice():
-    print("WIP")
-    print("")
-    prompt_user()
+def calculate_kill_chance(min_dmg, max_dmg, hp, hit_chance):
+    damage_values = np.arange(min_dmg, max_dmg + 1)
+    total_damage_vals = damage_values.size
 
+    kill_damage_vals = 0
+    for val in damage_values:
+        if val >= hp:
+            kill_damage_vals += 1
+
+    kill_chance_if_hit = kill_damage_vals / total_damage_vals
+    return hit_chance * kill_chance_if_hit * 100
 
 if __name__ == "__main__":
     main()
